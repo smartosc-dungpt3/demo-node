117e16cba1504e377cc3c9f04ad6bf5f23a5972a
smartosc-dungpt3 <dungpt3@smartosc.com>
2023-03-09 23:24:48 +0700

    first commit

--- src/app/extension/__tests__/etc/di.ts
+++
@@ -1,7 +0,0 @@
-import 'reflect-metadata' // Must have
-import { container } from 'tsyringe'
-import ResourceConnection from '../../../core/ResourceModel/ResourceConnection'
-import TestResourceModel from '../Model/ResourceModel/Test'
-
-container.register('ResourceConnectionInterface', { useClass: ResourceConnection })
-container.register('TestResourceModel', { useClass: TestResourceModel })
